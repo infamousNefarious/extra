926765bb283d34f07c3e08a4c0ea1ef721578c56
infamousNefarious <132144940+infamousNefarious@users.noreply.github.com>
2023-04-29 21:36:07 +0100

    Update names.lua

--- names.lua
+++ names.lua
@@ -1,1 +1,1 @@
-return {"Decuwu_RSG", "Rimuru_RSG", "sit.one", "xenixiyo", "Transsexual.", "Catualus", "Arenstoner"}
+return {"Decuwu_RSG", "Rimuru_RSG", "sit.one", "xenixiyo", "Transsexual.", "Catualus", "Arenstoner", "mikeywikey-"}
